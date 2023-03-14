07ae0cf33a6fa1c9c678f4034b1385a85ddcad39
mr-xevil <127093604+mr-xevil@users.noreply.github.com>
2023-03-14 16:00:20 +0600

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,36 @@
+/data/data/com.termux/files/usr/etc
+clear
+echo
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo -e '\033[1;96m' " |        Created by 'MR.TAMIM'       |"
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo -e '\033[1;96m' " |        Facebook: Tamim Billa       |"
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo
+echo -e '\033[1;96m' "😈😈😈😈😈😈😈😈😈😈😈😈😈😈"
+echo -e '\033[1;92m'
+echo "Installing.................."
+echo
+apt install python -y
+pip install lolcat
+pkg install figlet
+pkg install mpv -y
+echo
+rm /data/data/com.termux/files/usr/etc/bash.bashrc
+rm /data/data/com.termux/files/usr/etc/motd
+mv bash.bashrc /data/data/com.termux/files/usr/etc
+mv lib /data/data/com.termux/files/usr/etc
+cd ..
+rm -rf BannerX
+echo
+clear
+echo
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo -e '\033[1;96m' " |        Created by 'MrHacker-X'       |"
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo -e '\033[1;96m' " |        Instagram: @ 0hacker_x0       |"
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo
+figlet Finished | lolcat
+echo -e '\033[1;96m' "Restart your termux and see awesome look"
+echo
